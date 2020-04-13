70612c35ca07232a184e7ed3c9fa1df66ae4977a
wawerualbert58 <wawerualbert58@gmail.com>
2020-04-13 03:15:26 -0400

    start building estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,43 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+        const inputData = {
+            region: {
+                name: "Africa",
+                avgAge: 19.7,
+                avgDailyIncomeInUSD: 5,
+                avgDailyIncomePopulation: 0.71
+            },
+            periodType: "days",
+            timeToElapse: 58,
+            reportedCases: 674,
+            population: 66622705,
+            totalHospitalBeds: 1380614
+         }
+    };
+     return {
+         data: inputData,
+         impact: {
+            currentlyInfected: impact.currentlyInfected = reportedCases * 1,
+            infectionsByRequestedTime: impact.currentlyInfected * 512,
+    
+         },
+         severeImpact: {
+            currentlyInfected: severeImpact.currentlyInfected = reportedCases * 50,
+            infectionsByRequestedTime: severeImpact.currentlyInfected * 512,
+    
+         }
+     };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default covid19ImpactEstimator;
